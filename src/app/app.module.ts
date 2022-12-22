0d9314dbc20a6e56106b014935e12e03569cce03
myportafoliobryantnzapp <bry.tandazo@gmail.com>
2022-12-22 18:51:40 -0500

    Sensor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOptions } from '@awesome-cordova-plugins/device-motion/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ],
+  providers: [DeviceMotion,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOptions } from '@awesome-cordova-plugins/device-motion/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  x!: string;
+  y!: string;
+  z!: string;
+  timeStamp!: string;
+  id: any;
+
+  constructor(private deviceMotion: DeviceMotion) {
+    this.x = "-";
+    this.y = "-";
+    this.z = "-";
+    this.timeStamp = "-";
+  }
+
+  start(){
+
+    try{
+      var option: DeviceMotionAccelerometerOptions = 
+      {
+        frequency: 200
+      };
+
+      this.id = this.deviceMotion.watchAcceleration(option).subscribe((acc: DeviceMotionAccelerationData) => 
+      {
+        this.x = "" + acc.x;
+        this.y = "" + acc.y;
+        this.z = "" + acc.z;
+        this.timeStamp = "" + acc.timestamp;
+      }
+      );
+
+    }catch(err){
+      alert("Error" + err)
+    }
+    
+  }
+  stop(){
+    this.id.unsubscribe();
+  }
 
 }
